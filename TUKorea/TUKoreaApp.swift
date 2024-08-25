2addd1dc18e65472cadb038df0cc3ae8e3ac17b7
SeongJin Park <elloy21@naver.com>
2024-08-25 21:03:07 +0900

    Initial Commit

---
+++ TUKorea/TUKoreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TUKoreaApp.swift
+//  TUKorea
+//
+//  Created by SeongJin Park on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TUKoreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
